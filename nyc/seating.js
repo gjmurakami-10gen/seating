8127f06eb5fc8e3342691725ecd20f4e13616231
David Storch <david.storch@10gen.com>
2013-11-06 16:32:39 -0500

    better scaling and pop-up who sits where message

--- nyc/seating.js
+++ nyc/seating.js
@@ -1,13 +1,14 @@
 
 var NUM_SEATS = 164;
 
+var bySeat;
+
 $(document).ready(function () {
     // set up highlighting behavior
     for (var i = 1; i <= NUM_SEATS; i++) {
         highlightOnMouseenter(i);
     }
 });
-
 
 function highlightOnSearch(seatid) {
     // get positioning info from the seat number
@@ -32,13 +33,24 @@ function highlightOnMouseenter(seatid) {
     var seatid_str = '#seat_' + seatid;
 
     // mouseenter turns number red
-    $(seatid_str).mouseenter(function() {
+    $(seatid_str).mouseenter(function(e) {
         $(seatid_str).attr('fill', 'red');
+
+        var person = bySeat[seatid];
+        if (person) {
+            var overlay = $('<div></div>');
+            overlay.attr('class', 'seat_overlay alert alert-success');
+            overlay.css('left', e.pageX);
+            overlay.css('top', e.pageY);
+            overlay.html(person.first + ' ' + person.last);
+            $('body').append(overlay);
+        }
     });
 
     // mouseleave turns it black again
     $(seatid_str).mouseleave(function() {
         $(seatid_str).attr('fill', 'black');
+        $('.seat_overlay').remove();
     });
 }
 
